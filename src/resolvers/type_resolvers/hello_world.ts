ac9418adbce471b214aeb6dced2331ce558cd7ee
Dawood Ali <59256614+DawoodAli23@users.noreply.github.com>
2023-02-04 19:30:20 +0500

    hello world

--- src/resolvers/type_resolvers/hello_world.ts
+++ src/resolvers/type_resolvers/hello_world.ts
@@ -1,8 +1,8 @@
-import { FieldResolver, Resolver, Root } from 'type-graphql';
+import { FieldResolver, Resolver, Root } from "type-graphql";
 
-import { HelloWorldBase } from '../../models/dto/hello_world_base';
-import HelloDev from '../../models/types/hello_dev';
-import HelloWorld from '../../models/types/hello_world';
+import { HelloWorldBase } from "../../models/dto/hello_world_base";
+import HelloDev from "../../models/types/hello_dev";
+import HelloWorld from "../../models/types/hello_world";
 
 @Resolver(() => HelloWorld)
 export default class HelloWorldResolver {
@@ -10,7 +10,7 @@ export default class HelloWorldResolver {
   async helloDev(@Root() parent: HelloWorldBase): Promise<HelloDev> {
     return {
       id: 10,
-      name: 'Hello Dev!',
+      name: "Hello Wrold!",
     };
   }
 }
